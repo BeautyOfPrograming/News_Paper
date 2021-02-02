8f6721ee1ff171e77046fd9f78d76e3168440da6
AliMohammadi <Aliahmadmohammadi412@gmail.com>
2021-02-02 09:06:52 +0330

    adding admob 3

--- app/src/main/java/com/saberi/myenglishnewsagency/MainActivity.java
+++ app/src/main/java/com/saberi/myenglishnewsagency/MainActivity.java
@@ -23,9 +23,9 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.OnUserEarnedRewardListener;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
 import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
 import com.google.android.gms.ads.rewarded.RewardedAd;
 import com.google.android.gms.ads.rewarded.RewardedAdCallback;
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
@@ -39,17 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
     private RewardedAd rewardedAd;
 
-    RewardedVideoAd rewardedVideoAd;
 
-    private RewardedAd mRewardedAd;
     private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
@@ -59,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // load and show add
+        loadAD();
+        showAdd();
 
         AdView mAdView;
         mAdView = findViewById(R.id.adView);
@@ -186,6 +185,70 @@ public class MainActivity extends AppCompatActivity {
 //        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(TabLayout));
 
     }
+
+
+     private  void loadAD(){
+
+        this.rewardedAd = new RewardedAd(this,"ca-app-pub-3940256099942544/5224354917");
+        RewardedAdLoadCallback rewardedAdLoadCallback = new RewardedAdLoadCallback(){
+            @Override
+            public void onRewardedAdLoaded() {
+                super.onRewardedAdLoaded();
+
+                Log.d("load_success","loads successful");
+            }
+
+            @Override
+            public void onRewardedAdFailedToLoad(int i) {
+                super.onRewardedAdFailedToLoad(i);
+                Log.d("dose_not_success","dose not laod  successful");
+
+            }
+        };
+
+         this.rewardedAd.loadAd(new AdRequest.Builder().build(),
+                 rewardedAdLoadCallback);
+     }
+
+     private void showAdd(){
+
+        if (rewardedAd.isLoaded()){
+
+            RewardedAdCallback rewardedAdCallback = new RewardedAdCallback() {
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+
+                    Log.d("user rewarder","rewarded");
+
+                }
+
+                @Override
+                public void onRewardedAdOpened() {
+                    super.onRewardedAdOpened();
+
+                    Log.d("opens","opened");
+
+                }
+
+                @Override
+                public void onRewardedAdClosed() {
+                    super.onRewardedAdClosed();
+
+                    Log.d("closes","closed");
+
+                }
+            };
+
+
+           this.rewardedAd.show(this,rewardedAdCallback);
+        }
+        else {
+
+            Log.d("dose_not_ loaded","dose_not_ loaded");
+
+        }
+
+     }
 
 
 }
